ac52cb22dfdf71448901e1f2eacbc59266c896d7
Muhamad Nur Alfi Syahrin <muhammadnuralfi98@gmail.com>
2022-06-16 10:52:47 +0700

    Initialing Javascript, and adding minor change to HTML tag

---
+++ app.js
@@ -0,0 +1,14 @@
+// Initialing Variable
+let firstNameField = document.getElementById("first-name-field")
+let lastNameField = document.getElementById("last-name-field")
+let userEmailField = document.getElementById("email-field")
+let userPasswordField = document.getElementById("password-field")
+let confirmButton = document.getElementsByClassName("submit-button")
+
+// The Logic
+//   git config --global user.email "you@example.com"
+// git config --global user.name "Your Name"
+
+// Testing functionality
+console.log(firstNameField)
+console.log(confirmButton)
