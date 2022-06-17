e80d1d5a6b677ceb0b3baee6d2ff028b175b1d6e
Muhamad Nur Alfi Syahrin <30311659+TahoeBoelat@users.noreply.github.com>
2022-06-17 15:47:01 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,118 @@
+// Learning from
+// https://developer.mozilla.org/en-US/docs/Learn/Forms/Form_validation#validating_forms_using_javascript
+
 // Initialing Variable
-let firstNameField = document.getElementById("first-name-field")
-let lastNameField = document.getElementById("last-name-field")
-let userEmailField = document.getElementById("email-field")
-let userPasswordField = document.getElementById("password-field")
-let confirmButton = document.getElementsByClassName("submit-button")
+const form = document.getElementsByTagName("form")[0];
 
-// The Logic
-//   git config --global user.email "you@example.com"
-// git config --global user.name "Your Name"
+const firstNameField = document.getElementById("first-name-field");
+const firstNameFieldError = document.querySelector(
+  "#first-name-field + span.alert-dialog"
+);
 
-// Testing functionality
-console.log(firstNameField)
-console.log(confirmButton)
+const lastNameField = document.getElementById("last-name-field");
+const lastNameFieldError = document.querySelector(
+  "#last-name-field + span.alert-dialog"
+);
+
+const userEmailField = document.getElementById("email-field");
+const userEmailFieldError = document.querySelector(
+  "#email-field + span.alert-dialog"
+);
+
+const userPasswordField = document.getElementById("password-field");
+const userPassFieldError = document.querySelector(
+  "#password-field + span.alert-dialog"
+);
+
+// Per Input Logic
+// First name input
+firstNameField.addEventListener("input", function (event) {
+  if (firstNameField.validity.valid) {
+    firstNameFieldError.textContent = "";
+  } else {
+    showErrorFirstName();
+  }
+});
+// Last name input
+lastNameField.addEventListener("input", function (event) {
+  if (lastNameField.validity.valid) {
+    lastNameFieldError.textContent = "";
+  } else {
+    showErrorLastName();
+  }
+});
+// Email input
+userEmailField.addEventListener("input", function (event) {
+  if (userEmailField.validity.valid) {
+    userEmailFieldError.textContent = "";
+  } else {
+    showErrorEmail();
+  }
+});
+// Password input
+userPasswordField.addEventListener("input", function (event) {
+  if (userPasswordField.validity.valid) {
+    userPassFieldError.textContent = "";
+  } else {
+    showErrorPassword();
+  }
+});
+
+// Form Logic
+form.addEventListener("submit", function (event) {
+  // First name condition
+  if (!firstNameField.validity.valid) {
+    showErrorFirstName();
+    event.preventDefault();
+  }
+  // Last name condition
+  if (!lastNameField.validity.valid) {
+    showErrorLastName();
+    event.preventDefault();
+  }
+  // Email input condition
+  if (!userEmailField.validity.valid) {
+    showErrorEmail();
+    event.preventDefault();
+  }
+  // Password input condition
+  if (!userPasswordField.validity.valid) {
+    showErrorPassword();
+    event.preventDefault;
+  }
+});
+
+// Error Logic
+// First name error function
+function showErrorFirstName() {
+  // First Name Input Error Logic
+  if (firstNameField.validity.valueMissing) {
+    firstNameFieldError.textContent = "First name cannot be empty";
+  }
+  firstNameFieldError.className = "alert-dialog";
+}
+// Last name error function
+function showErrorLastName() {
+  // Last name input error logic
+  if (lastNameField.validity.valueMissing) {
+    lastNameFieldError.textContent = "Last name cannot be empty";
+  }
+  lastNameFieldError.className = "alert-dialog";
+}
+// Email error function
+function showErrorEmail() {
+  if (userEmailField.validity.valueMissing) {
+    userEmailFieldError.textContent = "Email cannot be empty";
+  } else if (userEmailField.validity.typeMismatch) {
+    userEmailFieldError.textContent = "Looks like this is not an email";
+  }
+  userEmailFieldError.className = "alert-dialog";
+}
+// Password error function
+function showErrorPassword() {
+  if (userPasswordField.validity.valueMissing) {
+    userPassFieldError.textContent = "Password cannot be empty";
+  }
+  userPassFieldError.className = "alert-dialog";
+}
+// Testing
